d77d3fb1e8e6cac8ede6acba4b19bd6d672af5e9
renaldyhidayatt <renaldyhidayatt@gmail.com>
2019-09-20 01:54:45 +0700

    Create Callback Function

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 // Stock Market Portofolio
 const express = require('express')
 const app = express()
-
-//express-handlebar
 const exphbs  = require('express-handlebars');
-
 const path = require('path');
+const request = require('request');
 const PORT = process.env.PORT || 3000;
+
+
+// Api pk_82871c3fd6854fc68baf6a8b45cad5cd
+function call_api(finishedAPI){
+    request('https://cloud.iexapis.com/stable/stock/fb/quote?token=pk_82871c3fd6854fc68baf6a8b45cad5cd', { json: true}, (err,res,body) => {
+        if(err){return console.log(err);}
+        if (res.statusCode === 200){
+            finishedAPI(body);
+        };
+    });
+}
+
+
 
 // set Handlebars Middleware
 app.engine('handlebars', exphbs());
@@ -16,8 +27,10 @@ const otherstuff = "Hello There, this is other Stuff";
 
 // set Handlebars Route
 app.get('/', function (req, res) {
-    res.render('home', {
-        stuff: otherstuff
+    call_api(function(doneAPI){
+        res.render('home', {
+            stock: doneAPI
+        });
     });
 });
 
